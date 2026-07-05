ab10b3fa691a54ea7b5b2e16a75759ac4200de7d
Dennis Shlaih <dennisshlaih@gmail.com>
2026-07-04 20:35:56 -0400

    Add status page

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react'
-import {useSearchParams} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 function SearchBar() {
     const [searchParams, setSearchParams] = useSearchParams();
--- src/pages/StatusPage.tsx
+++ src/pages/StatusPage.tsx
@@ -1,6 +1,58 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams, useSearchParams, Link } from "react-router-dom";
+import { fetchItems } from "../services/api";
+import type { Item } from "../types/Item";
+
 function StatusPage() {
+    const { status } = useParams();
+
+    const [searchParams] = useSearchParams();
+    const query = searchParams.get("q") ?? "";
+    const {
+        data,
+        isPending,
+        isError
+    } = useQuery<Item[]>({
+        queryKey: ["items"],
+        queryFn: fetchItems
+    });
+
+    if (isPending) return <p>Loading...</p>;
+    if (isError) return <p>Error loading items</p>;
+    if (!data) return <p>No data</p>;
+
+    const statusFiltered = data.filter((item) => item.status === status);
+
+    const filteredData = statusFiltered.filter((item) =>
+        item.title.toLowerCase().includes(query.toLowerCase())
+    );
+
     return (
-        <h1>Status Page</h1>
+        <main className="p-6">
+            <h1 className="text-3xl font-bold mb-4">
+                Status: {status}
+            </h1>
+
+            <div className="space-y-4">
+                {filteredData.map((item) => (
+                    <Link
+                        to={`/items/${item.id}`}
+                        key={item.id}
+                        className="block"
+                    >
+                        <div className="p-4 border rounded">
+                            <h2 className="font-semibold">
+                                {item.title}
+                            </h2>
+                            <p>{item.creator}</p>
+                            <p>{item.year}</p>
+                            <p>{item.genre}</p>
+                            <p>Status: {item.status}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </main>
     );
 }
 
